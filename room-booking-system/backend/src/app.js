a7705303a40db8e90059f9ea969ddaf3b7df8d73
himanshu7437 <himanshujangra368@gmail.com>
2026-03-05 18:22:46 +0530

    Backend - server - add app.js

---
+++ room-booking-system/backend/src/app.js
@@ -0,0 +1,29 @@
+import express from 'express'
+import cors from 'cors'
+import rateLimit from 'express-rate-limit'
+
+const app = express();
+
+/* ----------------- Middlewares ----------------- */
+
+// Parse JSON requests
+app.use(express.json());
+
+// Enable CORS
+app.use(cors());
+
+// Basic rate limiting
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+});
+
+app.use(limiter);
+
+/* ----------------- Health Route ----------------- */
+
+app.get("/", (req, res) => {
+  res.send("API Running");
+});
+
+export default app;
